d175a01413398372d7803626602583e368b81839
arslanismail <arslanismail840@gmail.com>
2020-04-03 01:28:11 +0500

    adding develop branch with production ready envioronment

--- src/api/v1/controllers/index.ts
+++ src/api/v1/controllers/index.ts
@@ -1,7 +1,3 @@
 import HealthController from './health.controller';
-import CountryController from './country.controller';
-import AuthorizationController from './authorization.controller';
 
 export { HealthController };
-export { CountryController };
-export { AuthorizationController };

77af95c892a3e8728ff97572d9b2e9324523ef62
arslanismail <arslanismail840@gmail.com>
2020-04-03 05:03:41 +0500

    Resolve Conflicts on testing

--- src/config/middlewares/index.ts
+++ src/config/middlewares/index.ts
@@ -2,7 +2,6 @@ import { Application } from 'express';
 import * as express from 'express';
 import parsers from './parsers.middleware';
 import custom from './custom.middleware';
-import ApiDoc from '../api.docs';
 import headers from './header.middleware';
 
 // Web Related Middleware Depends on req.session (express-session/cookie-session)
@@ -12,7 +11,6 @@ const config = (app: Application) => {
 	parsers.config(app);
 	custom.config(app);
 	app.use(express.static('public'));
-	ApiDoc.publish(app);
 	headers.config(app);
 };
 

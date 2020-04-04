457c8abad57379577c5334c4b8ee5259470efdc2
arslanismail <arslanismail840@gmail.com>
2020-04-04 23:45:01 +0500

    remove content-type requirement

--- src/config/middlewares/index.ts
+++ src/config/middlewares/index.ts
@@ -2,7 +2,7 @@ import { Application } from 'express';
 import * as express from 'express';
 import parsers from './parsers.middleware';
 import custom from './custom.middleware';
-import headers from './header.middleware';
+// import headers from './header.middleware';
 
 // Web Related Middleware Depends on req.session (express-session/cookie-session)
 // import lusca from './lusca.middleware';
@@ -11,7 +11,7 @@ const config = (app: Application) => {
 	parsers.config(app);
 	custom.config(app);
 	app.use(express.static('public'));
-	headers.config(app);
+	// headers.config(app);
 };
 
 export default { config };

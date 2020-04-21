731550b2b55a3cf7284d8c014b1e5edbf44c524a
asadrana1122 <laravel260@gmial.com>
2020-04-22 02:52:20 +0500

    Migration and Seeders

--- src/api/v1/services/migration.service.ts
+++ src/api/v1/services/migration.service.ts
@@ -14,27 +14,22 @@ class MigrationService implements IMigrationService {
 			'CREATE TABLE IF NOT EXISTS photos(id serial primary key,file character varying(500));'
 		);
 		await db.raw(
-			'CREATE TABLE IF NOT EXISTS adress(id serial primary key,city character varying(500),street character varying(500),country character varying(500));'
+			'CREATE TABLE IF NOT EXISTS address(id serial primary key,city character varying(500),street character varying(500),country character varying(500));'
 		);
 		await db
 			.raw(
-				'CREATE TABLE IF NOT EXISTS users(id serial primary key,role_id integer references roles(id) ,photo_id integer references photos(id),adress_id integer references adress(id),is_active integer Null,email character varying(100) Null,date_joined date NOT NULL DEFAULT current_date);'
+				'CREATE TABLE IF NOT EXISTS users(id serial primary key,fullName character varying(500),phoneNumber character varying(500),gender character varying(500),roleId integer references roles(id) ,photoId integer references photos(id),addressId integer references address(id),is_active integer Null,email character varying(100) Null,joinDate date NOT NULL DEFAULT current_date);'
 			)
 			.then(() => {
 				const result = { status: 200, data: 'Tables created successfully' };
-				// console.log(result);
+				console.log(result);
 				return result;
 			});
 	}
 	async seeds(): Promise<any> {
-		// const name = faker.name.firstName();
-		// const file = faker.image.avatar();
-
-		// const street = faker.address.streetAddress();
-		// const city = faker.address.city();
-		// const country = faker.address.country();
-
 		this.role();
+		this.address();
+		this.photos();
 		this.user();
 	}
 	async role(): Promise<any> {
@@ -44,30 +39,62 @@ class MigrationService implements IMigrationService {
 			const result = await db('roles').insert(data);
 			console.log('roles seeder has run');
 		} else {
-			console.log('role table already have data');
+			console.log('roles table already have data');
 		}
+	}
+
+	async photos(): Promise<any> {
+		const fileName = faker.image.avatar();
+		const result = await db('photos').insert({ file: fileName });
+		console.log('photos seeder has run');
+	}
+	async address(): Promise<any> {
+		const street = faker.address.streetAddress();
+		const city = faker.address.city();
+		const country = faker.address.country();
+		const result = await db('address').insert({
+			street: street,
+			city: city,
+			country: country,
+		});
+		console.log('Address seeder has run');
 	}
 	async user(): Promise<any> {
 		const email = faker.internet.email();
 		const date = faker.date.recent();
 		const name = faker.name.firstName();
+		const phone = faker.phone.phoneNumber();
 		const roles = await db('roles').select('id');
+		const photos = await db('photos').select('id');
+		const address = await db('address').select('id');
 		const roleIds: any = [];
 		roles.forEach((element) => {
 			roleIds.push(element.id);
+		});
+		const photoIds: any = [];
+		photos.forEach((element) => {
+			photoIds.push(element.id);
+		});
+		const addressIds: any = [];
+		address.forEach((element) => {
+			addressIds.push(element.id);
 		});
 
 		const user = await db('users').select();
 
 		const result = await db('users').insert({
+			fullname: name,
+			phonenumber: phone,
+			gender: 'Male',
 			email: email,
 			is_active: 1,
-			role_id: faker.random.arrayElement(roleIds),
-			photo_id: 1,
-			adress_id: 1,
-			date_joined: date,
+			roleid: faker.random.arrayElement(roleIds),
+			photoid: faker.random.arrayElement(photoIds),
+			addressid: faker.random.arrayElement(addressIds),
+			joindate: date,
 		});
-		console.log(result);
+		console.log('user seeder');
 	}
 }
+
 export default MigrationService;
